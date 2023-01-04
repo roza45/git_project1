f6d73b0c2ccba0e27a3429b807a7e9487e8ab785
roza45 <118614935+roza45@users.noreply.github.com>
2023-01-04 19:44:56 +0300

    Add files via upload

--- zastavka.py
+++ zastavka.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pygame
 from random import randint
-
+deed = ''
 pygame.init()
 fontUI = pygame.font.Font(None, 30)
 def load_image(name, colorkey='black'):
@@ -17,26 +17,26 @@ def terminate():
     sys.exit()
 
 def start_screen():
-    intro_text = ["ИГРА НА ДВОИХ",
+    main_text = ["ИГРА НА ДВОИХ",
                   "Правила игры",
                   "Чемпионат",
                   "Играть Пинг-Понг",
                   "Играть Танки"]
-
+    chet1 = chet2 = 0
     fon = pygame.transform.scale(load_image('fon3.jpg'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
-    text_coord = 50
-    coord = []
-    for line in intro_text:
+    text_koord = 50
+    koord = []
+    for line in main_text:
         string_rendered = font.render(line, 1, pygame.Color('white'), (0, 50, 0))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        coord.append(intro_rect)
-        screen.blit(string_rendered, intro_rect)
+        text_rect = string_rendered.get_rect()
+        text_koord += 10
+        text_rect.top = text_koord
+        text_rect.x = 10
+        text_koord += text_rect.height
+        koord.append(text_rect)
+        screen.blit(string_rendered, text_rect)
 
 
     while True:
@@ -45,9 +45,9 @@ def start_screen():
                 terminate()
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
-                for rect in coord:
+                for rect in koord:
                     if rect.collidepoint(event.pos):
-                        kuda = coord.index(rect)
+                        kuda = koord.index(rect)
                         if kuda == 4:
                             tanki()
         pygame.display.flip()
@@ -71,17 +71,17 @@ class UI:
                 screen.blit(text, rect)
                 i += 1
 class Tank:
-    def __init__(self, color, px, py, direct, keyList):
+    def __init__(self, color, px, py, step, keyList):
         objects.append(self)
         self.type = 'tank'
 
         self.color = color
-        self.rect = pygame.Rect(px, py, TILE - 3, TILE - 3)
-        self.direct = direct
+        self.rect = pygame.Rect(px, py, tile - 3, tile - 3)
+        self.step = step
         self.moveSpeed = 2
         self.hp = 5
 
-        self.shotTimer = 0
+        self.shotime = 0
         self.shotDelay = 60
         self.bulletSpeed = 5
         self.bulletDamage = 1
@@ -97,42 +97,43 @@ class Tank:
         oldX, oldY = self.rect.topleft
         if keys[self.keyLEFT]:
             self.rect.x -= self.moveSpeed
-            self.direct = 3
+            self.step = 3
         elif keys[self.keyRIGHT]:
             self.rect.x += self.moveSpeed
-            self.direct = 1
+            self.step = 1
         elif keys[self.keyUP]:
             self.rect.y -= self.moveSpeed
-            self.direct = 0
+            self.step = 0
         elif keys[self.keyDOWN]:
             self.rect.y += self.moveSpeed
-            self.direct = 2
+            self.step = 2
 
         for obj in objects:
             if obj != self and self.rect.colliderect(obj.rect):
                 self.rect.topleft = oldX, oldY
 
-        if keys[self.keySHOT] and self.shotTimer == 0:
-            dx = DIRECTS[self.direct][0] * self.bulletSpeed
-            dy = DIRECTS[self.direct][1] * self.bulletSpeed
+        if keys[self.keySHOT] and self.shotime == 0:
+            dx = steps[self.step][0] * self.bulletSpeed
+            dy = steps[self.step][1] * self.bulletSpeed
             Bullet(self, self.rect.centerx, self.rect.centery, dx, dy, self.bulletDamage)
-            self.shotTimer = self.shotDelay
+            self.shotime = self.shotDelay
 
-        if self.shotTimer > 0: self.shotTimer -= 1
+        if self.shotime > 0:
+            self.shotime -= 1
 
     def draw(self):
         pygame.draw.rect(screen, self.color, self.rect)
 
-        x = self.rect.centerx + DIRECTS[self.direct][0] * 30
-        y = self.rect.centery + DIRECTS[self.direct][1] * 30
+        x = self.rect.centerx + steps[self.step][0] * 30
+        y = self.rect.centery + steps[self.step][1] * 30
         pygame.draw.line(screen, 'white', self.rect.center, (x, y), 4)
 
     def damage(self, value):
         self.hp -= value
         if self.hp <= 0:
             objects.remove(self)
-            print(self.color, 'dead')
-
+            global deed
+            deed = self.color
 
 class Bullet:
     def __init__(self, parent, px, py, dx, dy, damage):
@@ -169,59 +170,78 @@ class Block:
         pass
 
     def draw(self):
-        screen.blit(imgBrick, self.rect)
+        screen.blit(imB, self.rect)
 
     def damage(self, value):
         self.hp -= value
-        if self.hp <= 0: objects.remove(self)
-
+        if self.hp <= 0:
+            objects.remove(self)
 
 def tanki():
+    global deed
     for _ in range(50):
         while True:
-            x = randint(0, WIDTH // TILE - 1) * TILE
-            y = randint(0, HEIGHT // TILE - 1) * TILE
-            rect = pygame.Rect(x, y, TILE, TILE)
+            x = randint(0, WIDTH // tile - 1) * tile
+            y = randint(0, HEIGHT // tile - 1) * tile
+            rect = pygame.Rect(x, y, tile, tile)
             fined = False
             for obj in objects:
-                if rect.colliderect(obj.rect): fined = True
+                if rect.colliderect(obj.rect):
+                    fined = True
 
-            if not fined: break
+            if not fined:
+                break
 
-        Block(x, y, TILE)
+        Block(x, y, tile)
     play = True
     while play:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 play = False
 
-        for bullet in bullets: bullet.update()
-        for obj in objects: obj.update()
+        for bullet in bullets:
+            bullet.update()
+        c = 0
+        for obj in objects:
+            if obj.type == 'tank':
+                c += 1
+            obj.update()
         ui.update()
         screen.fill('black')
-        for bullet in bullets: bullet.draw()
-        for obj in objects: obj.draw()
-        ui.draw()
-        pygame.display.update()
-        clock.tick(FPS)
+        if c != 2:
+            screen.fill((0, 0, 0))
+            font = pygame.font.Font(None, 50)
+            text = font.render(f'deed {deed}', True, 'green')
+            screen.blit(text, (250, 250))
+            pygame.display.update()
+        else:
+            for bullet in bullets:
+                bullet.draw()
+            for obj in objects:
+                obj.draw()
+            ui.draw()
+            pygame.display.update()
+            clock.tick(FPS)
+    start_screen()
+
 
 
 FPS = 50
-STEP = 5
-TILE =32
+steps = 5
+tile =32
 size = WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 objects = []
 bullets = []
 imageblock = load_image('box.png')
-imgBrick = pygame.transform.scale(imageblock, (TILE, TILE))
+imB = pygame.transform.scale(imageblock, (tile, tile))
 
 
 tile_width = tile_height = 50
-Tank('blue', 100, 275, 0, (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, pygame.K_SPACE))
-Tank('red', 650, 275, 0, (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, pygame.K_RETURN))
-DIRECTS = [[0, -1], [1, 0], [0, 1], [-1, 0]]
+Tank('blue', 90, 260, 0, (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, pygame.K_SPACE))
+Tank('red', 640, 260, 0, (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, pygame.K_RETURN))
+steps = [[0, -1], [1, 0], [0, 1], [-1, 0]]
 ui = UI()
 
 
